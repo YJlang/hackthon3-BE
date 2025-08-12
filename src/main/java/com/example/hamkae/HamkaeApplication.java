3a33a96768296de7199a3bde487d6e1e7aaa1d2e
fff-000 <chosm0168@gmail.com>
2025-08-12 20:24:35 +0900

    first

---
+++ src/main/java/com/example/hamkae/HamkaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.hamkae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamkaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamkaeApplication.class, args);
+	}
+
+}
