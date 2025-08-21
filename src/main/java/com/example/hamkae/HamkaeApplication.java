466bb7fbc4bfe8067cf41e233f0e38ec603ae693
fff-000 <chosm0168@gmail.com>
2025-08-21 15:07:37 +0900

    1

--- src/main/java/com/example/hamkae/HamkaeApplication.java
+++ src/main/java/com/example/hamkae/HamkaeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HamkaeApplication {
+public class gitHamkaeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HamkaeApplication.class, args);
